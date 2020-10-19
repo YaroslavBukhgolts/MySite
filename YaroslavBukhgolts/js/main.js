3ad537291f211fcdeb52b6a0700cc3d1185ed3f5
Yaroslav <yaroslav.bukhgolts@yandex.ru>
2020-10-19 18:29:02 +0300

    Добавил скрипт и файл php

--- YaroslavBukhgolts/js/main.js
+++ YaroslavBukhgolts/js/main.js
@@ -7,3 +7,23 @@ var mySwiper = new Swiper(".swiper-container", {
     clickable: true,
   },
 });
+
+// обработчик формы
+$(document).ready(function(){
+  $('.form-button').click(function(){
+    let formName = $('.input-name').val();
+    let formTel = $('.input-tel').val();
+    $.ajax({
+      url: 'form.php',
+      type: 'post',
+      dataType: 'json',
+      data: {
+        'input-name': input-name,
+        'input-tel': input-tel
+      },
+      success: function (data){
+        $('.messages').html(data.result);
+      }
+    });
+  });
+});
